30184ec982eefa1abb69e52bc171ac7111b43afd
Ian-Mburu <ianongaruiya@gmail.com>
2025-05-05 22:09:39 +0300

    dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,12 +1,35 @@
 import React from "react";
+import '../styles/Dashboard/dashboard.css'
+import { 
+  FiHome, FiPieChart, FiDollarSign, FiTarget, 
+  FiBell, FiCreditCard, FiTrendingUp, FiUser, FiSettings 
+} from 'react-icons/fi';
+import { FaMoneyBills } from "react-icons/fa6";
+
 
 
 const Dashboard = () => {
 
 
   return (
-    <div className="p-6 space-y-8">
-      Dashboard
+    <div className="dash-1">
+      <div className="header-1">
+        <h5>Mali Sana</h5>
+      </div>
+
+      <div className="lists">
+        <a href="home"> <FiHome /> Home</a>
+        <a href="home"> <FiPieChart /> Budget</a>
+        <a href="home"> <FiDollarSign /> Transactions</a>
+        <a href="Savings"> <FiTarget /> Savings</a>
+        <a href="bills"> <FaMoneyBills /> Bills</a>
+        <a href="home"> <FiCreditCard /> Goals</a>
+        <a href="investments"> <FiTrendingUp /> Investments</a>
+        <a href="home">Login</a>
+        <a href="home"> <FiUser /> Profile</a>
+        <a href="home"> <FiSettings /> Settings</a>
+        <FiBell /> 
+      </div>
     </div>
   );
 };
